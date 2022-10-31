1813060e3e304053cdada6be27e5056b9d7b22f7
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-10-31 21:08:44 +0100

    [add] window size

--- flowtime/ContentView.swift
+++ flowtime/ContentView.swift
@@ -8,28 +8,42 @@
 
 import SwiftUI
 
+enum WindowSize {
+    static let width: CGFloat = 300;
+    static let height: CGFloat = 200;
+}
+
+enum ButtonSize {
+    static let width: CGFloat = WindowSize.width;
+    static let height: CGFloat = WindowSize.height;
+}
+
 struct ContentView: View {
     @ObservedObject var watch: StopWatch = StopWatch();
     @State var isRunning = false;
     @State var isRelaxing = false;
 
+
     var body: some View {
+
 //        isRelaxing ?
         VStack {
             Text("\(watch.duration.minuteSecond)")
-                    .font(.title)
+                    .font(.system(size: 64))
             HStack {
-                isRunning ? Button("Stop", action: stop)
+                isRunning ? Button("Stop",action: stop)
                         : Button("Start", action: start)
                 Button("Reset", action: reset)
             }
-                    .font(.title2)
+                    .font(.title)
             Button("Riposati per \(watch.pause.minuteSecond)", action: relax)
 //        } : VStack {
 
         }
+                .frame(minWidth: WindowSize.width, minHeight: WindowSize.height)
                 .padding(8)
     }
+
 
     func relax() {
     }
