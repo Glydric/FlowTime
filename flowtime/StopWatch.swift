c807debfb7be0d363b5563d8ded29678747a76ea
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-10-31 16:54:39 +0100

    add StopWatch.swift

---
+++ flowtime/StopWatch.swift
@@ -0,0 +1,17 @@
+//
+// Created by Leonardo Migliorelli on 31/10/22.
+//
+
+import Foundation
+
+class StopWatch{
+    let begin: Date;
+
+    public init(){
+        self.begin = Date.now;
+    }
+
+    public func getDiffFromNow(){
+
+}
+}
