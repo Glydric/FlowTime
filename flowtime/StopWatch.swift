70b205d134e8b9d16de27266426bcf86703f685e
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-11-01 18:28:43 +0100

    [add] timeinterval extension in separate file

--- flowtime/StopWatch.swift
+++ flowtime/StopWatch.swift
@@ -4,28 +4,7 @@
 
 import Foundation
 
-extension TimeInterval {
-    var hourMinuteSecond: String {
-        String(format: "%d:%02d:%02d", hour, minute, second)
-    }
-    var minuteSecond: String {
-        hour == 0
-                ? String(format: "%02d:%02d", minute, second)
-                : hourMinuteSecond
-    }
-    var hour: Int {
-        Int((self / 3600).truncatingRemainder(dividingBy: 3600))
-    }
-    var minute: Int {
-        Int((self / 60).truncatingRemainder(dividingBy: 60))
-    }
-    var second: Int {
-        Int(truncatingRemainder(dividingBy: 60))
-    }
-    var millisecond: Int {
-        Int((self * 1000).truncatingRemainder(dividingBy: 1000))
-    }
-}
+
 
 
 class StopWatch: ObservableObject {
