bca0611289fc8d34c46a95bd3592413ea8038c5b
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-11-01 18:28:00 +0100

    [removed] buttonsize, add relative UI and using reset in relax

--- flowtime/MainView.swift
+++ flowtime/MainView.swift
@@ -9,15 +9,9 @@
 import SwiftUI
 
 enum WindowSize {
-    static let width: CGFloat = 300;
-    static let height: CGFloat = 200;
+    static let width: CGFloat = 350;
+    static let height: CGFloat = 250;
 }
-
-enum ButtonSize {
-    static let width: CGFloat = WindowSize.width;
-    static let height: CGFloat = WindowSize.height;
-}
-
 
 struct MainView: View {
     @ObservedObject var watch: StopWatch = StopWatch();
@@ -25,35 +19,47 @@ struct MainView: View {
     @Binding var isRelaxing: Bool;
 
     var body: some View {
+        GeometryReader { geometry in
 
-        VStack {
-            Text("\(watch.duration.minuteSecond)")
-                    .font(.system(size: 64))
-            HStack {
-                isRunning ? Button(action: stop, label: {
-                    Image(systemName: "pause")
-                    Text("Pause")
-                })
-                        : Button(action: start, label: {
-                    Image(systemName: "play")
-                    Text("Start")
-                })
-                Button(action: reset, label: {
-                    Image(systemName: "arrow.uturn.backward")
-                    Text("Reset")
-                })
+            VStack {
+                Text("\(Int(geometry.size.width))x\(Int(geometry.size.height)) \(Int(calcFontTitle(size: geometry.size)))")
+                Text("\(watch.duration.minuteSecond)")
+                        //                        .font(.system(size: 64))
+                        .font(.system(size: calcFontTitle(size: geometry.size)))
+                HStack {
+                    isRunning ?
+                            Button(action: stop, label: {
+                                Image(systemName: "pause.fill")
+                                Text("Pause")
+                            })
+                            :
+                            Button(action: start, label: {
+                                Image(systemName: "play.fill")
+                                Text("Start")
+                            })
+                    Button(action: reset, label: {
+                        Image(systemName: "gobackward")
+                        Text("Reset")
+                    })
+                }
+                Button(action: relax) {
+                    Image(systemName: "cup.and.saucer.fill")
+                    Text("Riposati per \(watch.pause.minuteSecond)")
+                }
             }
-//                    .font(.title)
-            Button("Riposati per \(watch.pause.minuteSecond)", action: relax)
-
+                    .padding(8)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
                 .frame(minWidth: WindowSize.width, minHeight: WindowSize.height)
-                .padding(8)
     }
 
+    func calcFontTitle(size: CGSize) -> CGFloat {
+        min(size.width / 4, size.height / 3)
+    }
 
     func relax() {
         isRelaxing = true;
+        reset()
     }
 
     func reset() {
