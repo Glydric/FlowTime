df0547b61db5fa042c83238ce90a576f3aaa7f9e
leomiglio02 <leo@miglio.dev>
2022-11-04 21:53:12 +0100

    [add] total time

--- flowtime/flowtimeApp.swift
+++ flowtime/flowtimeApp.swift
@@ -11,13 +11,14 @@ import SwiftUI
 @main
 struct flowtimeApp: App {
     @State var relaxingTime: TimeInterval = 0;
+    @State var total: TimeInterval = 0;
 
     var body: some Scene {
         WindowGroup {
             if (relaxingTime != 0) {
                 RelaxView(time: $relaxingTime)
             } else {
-                MainView(relaxingTime: $relaxingTime)
+                MainView(relaxingTime: $relaxingTime, total: $total)
             }
         }
     }
