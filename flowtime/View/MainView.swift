833ef1b98d1453ef1c773a80a1861d37d18987a5
leomiglio02 <leo@miglio.dev>
2022-11-04 20:20:21 +0100

    [code clean]

--- flowtime/View/MainView.swift
+++ flowtime/View/MainView.swift
@@ -15,36 +15,36 @@ enum DefaultWindowSize {
 
 struct MainView: View {
     @ObservedObject var watch: StopWatch = StopWatch();
-    @State var isRunning = false;
     @Binding var relaxingTime: TimeInterval;
+    @State var total: TimeInterval = 0;
 
     var body: some View {
         GeometryReader { geometry in
-
             VStack {
 //                Text("\(Int(geometry.size.width))x\(Int(geometry.size.height)) \(Int(calcFontTitle(size: geometry.size)))")
                 Text("\(watch.duration.minuteSecond)")
                         .font(.system(size: calcFontTitle(size: geometry.size)))
                 HStack {
-                    isRunning ?
-                            Button(action: stop, label: {
-                                Image(systemName: "pause.fill")
-                                Text("Pause")
-                            })
-                            :
-                            Button(action: start, label: {
+                    watch.isPaused ?
+                            Button(action: watch.start, label: {
                                 Image(systemName: "play.fill")
                                 Text("Start")
                             })
-                    Button(action: reset, label: {
+                            :
+                            Button(action: watch.stop, label: {
+                                Image(systemName: "pause.fill")
+                                Text("Pause")
+                            })
+                    Button(action: watch.reset, label: {
                         Image(systemName: "gobackward")
                         Text("Reset")
                     })
                 }
-                Button(action: relax) {
+                Button(action: beginRelaxing) {
                     Image(systemName: "cup.and.saucer.fill")
                     Text("Riposati per \(watch.relaxDuration.minuteSecond)")
                 }
+                Text("Somma sessioni \(total.hourMinuteSecond)")
             }
                     .padding(8)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -56,23 +56,10 @@ struct MainView: View {
         min(size.width / 4, size.height / 3)
     }
 
-    func relax() {
+    func beginRelaxing() {
+        total = total.advanced(by: watch.relaxDuration)
         relaxingTime = watch.relaxDuration
-        reset()
+        watch.reset()
     }
 
-    func reset() {
-        watch.reset();
-        isRunning = false;
-    }
-
-    func stop() {
-        watch.stop();
-        isRunning = false;
-    }
-
-    func start() {
-        watch.start();
-        isRunning = true;
-    }
 }
