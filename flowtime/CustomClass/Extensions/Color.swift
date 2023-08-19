943fe47a40f136fb2c67954974ad17d9b705c8eb
Glydric <git@miglio.dev>
2023-08-19 11:06:04 +0200

    added Colorpicker and minor improvements

---
+++ flowtime/CustomClass/Extensions/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  FlowTime
+//
+//  Created by Leonardo Migliorelli on 18/08/23.
+//
+
+import SwiftUI
+
+extension Color{
+	var nsColor: NSColor {
+		NSColor(self)
+	}
+
+}
--- flowtime/Views/EditProfiles.swift
+++ flowtime/Views/EditProfiles.swift
@@ -9,88 +9,97 @@ import SwiftUI
 
 struct EditProfile: View {
 	@AppStorage("profilesString") var profiles = [Profile]()
-
-	@State var willPresent = false
-	@State var profile: Profile? {
+	
+	@State var addingProfile = false {
 		didSet {
-			if let profile {
-				title = profile.title
-				color = profile.color
-			}
-			willPresent = true
+			newProfile = Profile()
 		}
 	}
 	
-	@State var title: String = ""
-	@State var color: Color = .white
+	@State var newProfile: Profile = Profile()
 	
 	var body: some View {
 		VStack{
-			Grid(alignment: .top, verticalSpacing: 16) {
-				ForEach(profiles, id: \.self) { p in
-					GridRow{
-						// TODO add when implemented color
-//						Circle()
-//							.fill(p.color)
-//							.frame(width: 16, height: 16)
-						
-						Text(p.title)
-						
-						Spacer()
-						
-						Text("Record: \(p.record.minuteSecond)")
-						Spacer()
-						
-						Text("Total: \(p.total.minuteSecond)")
-						Spacer()
-						
-						Button(
-							action: { profile = p },
-							label: {
-								Image(systemName: "pencil")
-								Text("Edit Profile")
-							}
-						)
-						
-						if profiles.count > 1{
-							Button(
-								action: {profiles.removeFirst(p)},
-								label: {
-									Image(systemName: "trash")
-									Text("Delete Profile")
-								}
-							)
-						}
-					}
+			Grid(alignment: .center, verticalSpacing: 32) {
+				GridRow{
+					Text("")
+					Text("")
+					Text("Record")
+					Text("Total")
+				}
+				
+				ForEach(0..<profiles.count){ index in
+					createProfileGridRow(index)
+					Divider()
 				}
 			}
 			.scaledToFit()
 			.fixedSize(horizontal: false, vertical: true)
 			.padding(16)
+			.alert(
+				"Set the name",
+				isPresented: $addingProfile,
+				actions: {
+					TextField("Name", text: $newProfile.title)
+					//TODO add ColorPicker in adding
+					//						ColorPicker("Color", selection: $color)
+					
+					Button("Save") {
+						profiles.append(newProfile)
+						profiles.removeDuplicates()
+					}.disabled(newProfile.title.isEmpty)
+					
+					Button("Cancel", role: .cancel) { }
+				},
+				message: { }
+			)
 			
-			Button("Add"){ profile = Profile() }
-				.alert(
-					"Set the name",
-					isPresented: $willPresent,
-					actions: {
-						TextField("Name", text: $title)
-						//TODO add ColorPicker in adding
-//						ColorPicker("Color", selection: $color)
-						Button("Save") {
-							guard var profile else { return }
-							profiles.removeFirst(profile)
-							profile.color = color
-							profile.title = title
-							profiles.append(profile)
-							profiles.removeDuplicates()
-						}.disabled(title.isEmpty)
-						Button("Cancel", role: .cancel) { }
-					}, message: {
-						
-					}
-				)
+			Button("Add"){ addingProfile = true }
+				.padding(.bottom, 24)
 		}
 		.frame(minWidth: 480)
 		.scaledToFit()
+	}
+	
+	func createProfileGridRow(_ index: Int) -> some View {
+		var p: Profile { profiles[index] }
+		return GridRow{
+			Circle()
+				.fill(p.color) // fill to actual color
+				.frame(width: 32, height: 32, alignment: .center)
+				.padding(4) // gives the background a padding
+				.background(AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .pink]), center: .center).cornerRadius(32))
+				.overlay(
+					ColorPicker("",
+						selection: $profiles[index].color,
+						supportsOpacity: false
+					)
+					.labelsHidden()
+					.opacity(0.08)
+					.blur(radius: 10)
+					.frame(width: 16, height: 16)
+				)
+				.shadow(radius: 0.5)
+			
+			TextField("Required",
+				text: $profiles[index].title
+			)
+			.textFieldStyle(RoundedBorderTextFieldStyle())
+			
+			Text(p.record.minuteSecond)
+			
+			Text(p.total.minuteSecond)
+			
+			if profiles.count > 1 {
+				Button(
+					action: {profiles.removeFirst(p)},
+					label: {
+						Image(systemName: "trash")
+						Text("Delete")
+					}
+				)
+			}
+			
+		}
 	}
 }
